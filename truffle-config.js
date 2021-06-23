5553fac993f909042874ec2212746da60bb78c39
AbdulHannanButt <35297041+AbdulHannanButt@users.noreply.github.com>
2021-06-24 01:31:12 +0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,7 @@
 var infuraEndpoint = "https://rinkeby.infura.io/v3/43d83d69a1cd4abbbb5a21696eea8847";
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const privateKeys = [
-    "0x6d0f73b7d16103ae977274d8c2613b25953b6cf03fe12122aafe65f1289de3d2"
-  ]
+    ]
 module.exports = {
  networks: {
    etherscan: {
